f5c62b7f371c85ef26b9344b38e5c6c650465f3d
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2022-12-12 20:20:56 +0100

    Stworzono StringTest, Obiekt VS Wartość, Problem022

---
+++ src/Problem022.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Problem022 {
+    private static String encrypt(String text, int key) {
+        text = text.toUpperCase();
+        String encrypted = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char currentCharacter = text.charAt(i);
+            if (Character.isUpperCase(currentCharacter)) {
+                int characterIndex = currentCharacter - (char) ('A');
+                int characterShifted = (characterIndex + key) % 26 + (char) ('A');
+                encrypted += ((char) (characterShifted));
+            } else {
+                encrypted += currentCharacter;
+            }
+        }
+        return encrypted;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj tekst i przesunięcie");
+        String tekst = scanner.nextLine();
+        int n = scanner.nextInt();
+        System.out.println(encrypt(tekst, n));
+
+    }
+}
---
+++ src/ValuesVsReferences.java
@@ -0,0 +1,14 @@
+public class ValuesVsReferences {
+    public static void main(String[] args) {
+
+        //wartości
+        int a = 10;
+        boolean b = true;
+
+        //obiekty
+        String powitanie = "Hejo";
+
+        //tablica sama w sobie jest obiektem
+        double[] reczywiste = new double[5];
+    }
+}
