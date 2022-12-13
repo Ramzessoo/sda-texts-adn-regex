a9273064a2ebe0f352a1557f4bbc2f6d30f0f2c3
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2022-12-13 11:48:03 +0100

    Dodanie komentarzy do Problemu IsPolindrom

--- src/IsPolindrom.java
+++ src/IsPolindrom.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class IsPolindrom {
+
+    //utworzenie metody zwracającą boolean, czy słowo jest Polidronem, argumentem jest text
     private static boolean isPolindrom(String text) {
-        text.toLowerCase();
-        int length = text.length();
-        for (int i = 0; i < length / 2; i++) {
-            if (text.charAt(i) != text.charAt(length - 1 - i)) {
-                return false;
+        text.toLowerCase(); //tekst ujednolicamy - na małe litery
+        int length = text.length(); //do zmiennej przypisujemy długość tekstu
+        for (int i = 0; i < length / 2; i++) {  //przechodzimy po znakach do połowy tekstu
+            if (text.charAt(i) != text.charAt(length - 1 - i)) { //jesli znak o indeksie i nie jest równy znakowi o indeksie długość słowa minus 1 (bo jest 0, 1, 2...), minus i(które jest również od zera do połowy więc zmniejsze index z długość minus 1- do połowy długości -1)
+                return false;       //to zwracamy fałsz bo to znaczy że znaki się nie powtarzają
             }
         }
-        return true;
+        return true;  //wychodzimy z pętli i zwracamy true w każdym przeciwnym razie
     }
 
     public static void main(String[] args) {
