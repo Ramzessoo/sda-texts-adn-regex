03d8ac2d01e0207da68ee16fca0f6fa85b7b8ec5
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2022-12-13 19:02:30 +0100

    dodano Regex002

---
+++ src/regex/Regex003.java
@@ -0,0 +1,17 @@
+package regex;
+
+public class Regex003 {
+    public static void main(String[] args) {
+        RegexTester tester = new RegexTester("(\\+48)*5[0-9]{8}");
+
+        tester.validate("+48515332123");
+        tester.validate("515336293");
+        //false
+        tester.validate("51123");
+        tester.validate("615332123");
+        tester.validate("715332123");
+        tester.validate("+48715332123");
+        tester.validate("712123");
+        tester.validate("484848412342421");
+    }
+}
