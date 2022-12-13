91b693e71a32b8f603fecc422a62abd29e795ff1
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2022-12-13 11:34:57 +0100

    Dodanie komentarzy do Problemu

--- src/Problem022.java
+++ src/Problem022.java
@@ -1,15 +1,23 @@
-import java.util.Locale;
 import java.util.Scanner;
+
 //Szyfr cezara
 public class Problem022 {
+
+    //metoda zawracająca zaszyfrowaną wiadomość
     private static String encrypt(String text, int key) {
+        //tekst usera przypisany do zmienneh
         text = text.toUpperCase();
+        //deklaracja zmiennej do przechowywania zaszyforwanego tekstu
         String encrypted = "";
 
+        //pętla przechodząca po tekście
         for (int i = 0; i < text.length(); i++) {
+            //zmienna pomocnicza przechowywująca po kolei każdy znak o indeksie i
             char currentCharacter = text.charAt(i);
+
+            //wywołanie metody .usUpperCase - sprawdzając czy znak jest dużą literą sprawdzamy czy nie jest spacją bądź cyfrą - mądre
             if (Character.isUpperCase(currentCharacter)) {
-                int characterIndex = currentCharacter - (char) ('A');
+                int characterIndex = currentCharacter - (char) ('A'); //
                 int characterShifted = (characterIndex + key) % 26 + (char) ('A');
                 encrypted += ((char) (characterShifted));
             } else {
@@ -20,8 +28,11 @@ public class Problem022 {
     }
 
     public static void main(String[] args) {
+
+        //pobranie tekstu od usera
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj tekst i przesunięcie");
+        //pobranie wartości przeusnięcia kodu
         String tekst = scanner.nextLine();
         int n = scanner.nextInt();
         System.out.println(encrypt(tekst, n));
