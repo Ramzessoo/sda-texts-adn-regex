04ee3113daf0f9ae9150924bf7195b86ab2ea251
Ramzessoo <117750497+Ramzessoo@users.noreply.github.com>
2022-12-13 18:38:16 +0100

    dodanie klasy regex tester i poprawa regexów

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -1,36 +1,16 @@
 package regex;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/*
-Napisz Regex,
-który dopasowuje ciąg znaków, w który zaczyna się od 'p',
- po którym następuje cokolwiek, kończąc na 'q'.
-
- pdfgdfgdsfg
- pq
- pqqqqqqqqq
- p123423412342134q
- */
 public class Regex001 {
-    private static final Pattern pattern = Pattern.compile("p.*q");
 
     public static void main(String[] args) {
-        validate("pasdfsdfq");
-        validate("pq");
-        validate("pqqqq");
-        validate("p12421341234q");
-        validate("qqqqq");
-        validate("pasdfsadf");
+        RegexTester tester = new RegexTester("p.*q");
+
+        tester.validate("pasdfsdfq");
+        tester.validate("pq");
+        tester.validate("pqqqq");
+        tester.validate("p12421341234q");
+        tester.validate("qqqqq");
+        tester.validate("pasdfsadf");
     }
 
-    private static void validate(String text) {
-        Matcher matcher = pattern.matcher(text);
-        if (matcher.matches()) {
-            System.out.println("Prawda dla tekstu: " + text);
-        } else {
-            System.out.println("Fałsz dla tekstu: " + text);
-        }
-    }
 }
